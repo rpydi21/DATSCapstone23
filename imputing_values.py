9e12e7ea3e035f0088a765514816b01c0f853640
rpydi21 <rohith.pydi@gmail.com>
2023-11-03 14:56:56 -0400

    fixed encoding, now working on imputing

--- imputing_values.py
+++ imputing_values.py
@@ -3,8 +3,35 @@ import pandas as pd
 import numpy as np
 from data_processing import data_cleaned
 from sklearn.preprocessing import LabelEncoder
+
+#%%
+def label_encoding(variable):
+    if variable.dtype == 'object':
+        column = variable.name
+        variable[pd.isnull(variable)] = "NaN"
+
+        
+        le = LabelEncoder()
+        variable = pd.Series(le.fit_transform(variable))
+        label_encoders[column] = le
+        label_mapping = {label: index for index, label in enumerate(le.classes_)}
+        variable.unique()
+
+        if (label_mapping.get("NaN", "Missing") != "Missing"):
+            #replace label mapping value in variable for "NaN" with np.nan
+            variable.replace(label_mapping.get("NaN"), np.nan, inplace=True)
+        
+    return variable
+
+label_encoders = {}
+data_encoded = data_cleaned.apply(label_encoding)
+
 # %%
 from sklearn.neighbors import NearestNeighbors
+df = data_encoded.copy()
+
+row = df.iloc[209]
+row.isnull().any()
 def hot_deck_imputation(df, k_neighbors=1):
     # Iterate through each row with missing values
     for row_index, row in df.iterrows():
@@ -14,6 +41,7 @@ def hot_deck_imputation(df, k_neighbors=1):
             y = row.dropna()
             
             if len(X) > k_neighbors:
+                
                 # Fit a nearest neighbors model
                 nn = NearestNeighbors(n_neighbors=k_neighbors)
                 nn.fit(X)
@@ -29,20 +57,9 @@ def hot_deck_imputation(df, k_neighbors=1):
     
     return df
 #%%
-
-data = data_cleaned.select_dtypes(include=['object'])
-
-label_encoders = {}
-for column in data.columns:
-    if data[column].dtype == 'object':
-        le = LabelEncoder()
-        data[column] = le.fit_transform(data[column])
-        label_encoders[column] = le
-del column, label_encoders, le
-
 #count na in each column
-print(data.isnull().sum())
-imputed_data = hot_deck_imputation(data, k_neighbors=1)
+print(data_encoded.isnull().sum())
+imputed_data = hot_deck_imputation(data_encoded, k_neighbors=1)
 
 print(imputed_data)
 df = data.copy()
@@ -109,21 +126,24 @@ data.isnull().sum()
 data = label_encoding(data)
 
 #%%
-data = data_cleaned.select_dtypes(include=['object'])
-data = data.apply(label_encoding)
-test = data["chd"].apply(label_encoding)
-test.name
-
 def label_encoding(variable):
     column = variable.name
-    
+    variable[pd.isnull(variable)] = "NaN"
+
     label_encoders = {}
     le = LabelEncoder()
-    variable = le.fit_transform(variable)
+    variable = pd.Series(le.fit_transform(variable))
     label_encoders[column] = le
     label_mapping = {label: index for index, label in enumerate(le.classes_)}
-    variable = pd.Series(variable)
-    variable = variable.replace(label_mapping[np.nan], np.nan)
+    variable.unique()
+
+    if (label_mapping.get("NaN", "Missing") != "Missing"):
+        #replace label mapping value in variable for "NaN" with np.nan
+        variable.replace(label_mapping.get("NaN"), np.nan, inplace=True)
+    
     return variable
 
+data = data_cleaned.select_dtypes(include=['object'])
 
+data_encoded = data.apply(label_encoding)
+#%%

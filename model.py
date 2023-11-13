b5a3da8f35bcb0247406fb47c977efd0e9a0b9ca
rpydi21 <rohith.pydi@gmail.com>
2023-11-13 14:33:31 -0500

    Model updates w/ ROC and random forest

--- model.py
+++ model.py
@@ -1,14 +1,24 @@
-#%%
+# %%
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, f1_score
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, f1_score, roc_auc_score
+import matplotlib.pyplot as plt
+from sklearn.utils import class_weight, resample
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import SMOTENC
 
-data_imputed = pd.read_csv("../data/data_imputed.csv")
-data = data_imputed.copy()
-data.dropna(inplace = True)
+data = pd.read_csv("../data/data_imputed.csv")
+#drop values where other_cancer is equal to don't know
+data = data[data.other_cancer != "Don't know / Not Sure / Refused / Missing"]
+
+#get index of categorical features
+cat_cols = data.select_dtypes(include='object').columns
+cat_cols_idx = [col in cat_cols for col in data.columns]
 
 label_encoders = {}
 for column in data.columns:
@@ -18,37 +28,110 @@ for column in data.columns:
         label_encoders[column] = le
 del column, label_encoders, le
 
-X = data.drop('chd', axis=1)  # Features (all columns except the target)
-y = data['chd']  # Target variable
+target = 'other_cancer'
+X = data.drop(target, axis=1)  # Features (all columns except the target)
+y = data[target]  # Target variable
 
-#%%
-
-#try clustering
-
-#%% Run decision tree without class weighting (f1 = 0.5824665001243977)
-print("Decision Tree without class weighting")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-clf = DecisionTreeClassifier()
-clf.fit(X_train, y_train)
+# %%
+def decision_tree(X_train, X_test, y_train, y_test, class_weight=None, return_clf = False):
+    if class_weight is None:
+        clf = DecisionTreeClassifier()
+    else:
+        clf = DecisionTreeClassifier(class_weight=class_weight)
+    # Train the model
+    clf.fit(X_train, y_train)
 
-y_pred = clf.predict(X_test)
+    # Make predictions
+    y_pred = clf.predict(X_test)
+    y_pred_proba = clf.predict_proba(X_test)[:, 1]
 
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
+    model_results(y_pred, y_test, y_pred_proba)
 
-#calculate f1 score
-f1 = f1_score(y_test, y_pred, average='macro')
-print("F1 score:", f1)
+    if return_clf:
+        return clf
+    
 
-report = classification_report(y_test, y_pred)
-print(report)
 
-cm = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:")
-print(cm)
+def random_forest(X_train, X_test, y_train, y_test, class_weight=None, return_clf = False):
+    if class_weight is None:
+        clf = RandomForestClassifier(n_estimators=100, random_state=42)
+    else:
+        clf = RandomForestClassifier(n_estimators=100, random_state=42, class_weight=class_weight)
 
-# %% Export Decision Tree to png
+    # Train the model
+    clf.fit(X_train, y_train)
+    y_pred_proba = clf.predict_proba(X_test)[:, 1]
+
+    # Make predictions
+    y_pred = clf.predict(X_test)
+
+    model_results(y_pred, y_test, y_pred_proba)
+
+    if return_clf:
+        return clf
+        
+def model_results(y_pred, y_test, y_pred_proba):
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy: {accuracy:.2f}")
+
+    #calculate f1 score
+    f1 = f1_score(y_test, y_pred, average='macro')
+    print("F1 score:", f1)
+
+    report = classification_report(y_test, y_pred)
+    print(report)
+
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion Matrix:")
+    print(cm)
+    
+    roc_auc = roc_auc_score(y_test, y_pred_proba)
+    print('ROC AUC score:', roc_auc)
+
+def feature_importance(clf):
+    importances = clf.feature_importances_
+
+    # Print the name and importance of each feature 
+    for feature_name, importance in zip(X.columns, importances):
+        print(f"{feature_name}: {importance}")
+    #sort features by importance
+    sorted(zip(importances, X.columns), reverse=True)
+
+    # Sort the feature importances in descending order
+    indices = np.argsort(importances)[::-1]
+
+    # Rearrange feature names so they match the sorted feature importances
+    names = [X.columns[i] for i in indices]
+
+    # Create plot
+    plt.figure()
+
+    # Create plot title
+    plt.title("Feature Importance")
+
+    # Add bars
+    plt.bar(range(X.shape[1]), importances[indices])
+
+    # Add feature names as x-axis labels
+    plt.xticks(range(X.shape[1]), names, rotation=90)
+
+    # Show plot
+    plt.show()
+        
+
+# %% [markdown]
+# 
+
+# %%
+print("Decision Tree without class weighting")
+decision_tree(X_train, X_test, y_train, y_test)
+
+print("\nRandom Forest without class weighting")
+random_forest(X_train, X_test, y_train, y_test)
+
+# %%
 # from sklearn.tree import export_graphviz
 # import graphviz
 # import pydotplus
@@ -57,183 +140,82 @@ print(cm)
 # graph = pydotplus.graph_from_dot_data(dot_data)
 # graph.write_png('decision_tree.png')
 
-#%% Run decision tree with class weighting (f1 = 0.5798864818524276)
-print("Decision Tree with class weighting")
-from sklearn.utils import class_weight
-
-# Split the data into features and target variable
-X = data.drop("chd", axis=1)
-y = data["chd"]
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+# %%
 # Calculate class weights
 class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
 class_weights_dict = dict(enumerate(class_weights))
 
-# Fit a decision tree classifier to the training data with class weighting
-clf = DecisionTreeClassifier(class_weight=class_weights_dict)
-clf.fit(X_train, y_train)
+print("Decision Tree with class weighting")
+decision_tree(X_train, X_test, y_train, y_test, class_weights_dict)
 
-# Make predictions on the testing data
-y_pred = clf.predict(X_test)
+print("\nRandom Forest without class weighting")
+random_forest(X_train, X_test, y_train, y_test, class_weights_dict)
 
-# Evaluate the performance of the model
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-
-#calculate f1 score
-f1 = f1_score(y_test, y_pred, average='macro')
-print("F1 score:", f1)
-
-report = classification_report(y_test, y_pred)
-print(report)
-
-cm = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:")
-print(cm)
-
-
-#%% Run undersampling to balance the data (f1 = 0.5303245214531039)
-print("Undersampling")
-from imblearn.under_sampling import RandomUnderSampler
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+# %%
 # Undersample the majority class
 rus = RandomUnderSampler(random_state=42)
 X_resampled, y_resampled = rus.fit_resample(X_train, y_train)
 
-# Fit a decision tree classifier to the resampled data with class weighting
-clf_resampled = DecisionTreeClassifier()
-clf_resampled.fit(X_resampled, y_resampled)
+print("Decision Tree with Undersampling")
+decision_tree(X_resampled, X_test, y_resampled, y_test)
 
-# Make predictions on the testing data
-y_pred_resampled = clf_resampled.predict(X_test)
+print("\nRandom Forest with Undersampling")
+random_forest(X_resampled, X_test, y_resampled, y_test)
 
-# Evaluate the performance of the model
-accuracy_resampled = accuracy_score(y_test, y_pred_resampled)
-print("Accuracy (resampled):", accuracy_resampled)
-
-#calculate f1 score
-f1 = f1_score(y_test, y_pred_resampled, average='macro')
-print("F1 score:", f1)
-
-report_resampled = classification_report(y_test, y_pred_resampled)
-print(report_resampled)
-
-cm_resampled = confusion_matrix(y_test, y_pred_resampled)
-print("Confusion Matrix (resampled):")
-print(cm_resampled)
-
-#%% Run oversampling to balance the data (f1 = 0.5816778196480517)
-print("Oversampling")
-from imblearn.over_sampling import RandomOverSampler
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
+# %%
 # Oversample the minority class
 ros = RandomOverSampler(random_state=42)
 X_resampled, y_resampled = ros.fit_resample(X_train, y_train)
 
-# Fit a decision tree classifier to the resampled data
-clf_resampled = DecisionTreeClassifier()
-clf_resampled.fit(X_resampled, y_resampled)
+print("Decision Tree with Oversampling")
+decision_tree(X_resampled, X_test, y_resampled, y_test)
 
-# Make predictions on the testing data
-y_pred_resampled = clf_resampled.predict(X_test)
+print("\nRandom Forest with Oversampling")
+random_forest(X_resampled, X_test, y_resampled, y_test)
 
-# Evaluate the performance of the model
-accuracy_resampled = accuracy_score(y_test, y_pred_resampled)
-print("Accuracy (resampled):", accuracy_resampled)
-
-#calculate f1 score
-f1 = f1_score(y_test, y_pred_resampled, average='macro')
-print("F1 score:", f1)
-
-report_resampled = classification_report(y_test, y_pred_resampled)
-print(report_resampled)
-
-cm_resampled = confusion_matrix(y_test, y_pred_resampled)
-print("Confusion Matrix (resampled):")
-print(cm_resampled)
-
-#%% Run SMOTENC to balance the data (f1 = 0.6565410412248198))
-print("SMOTE-NC")
-# Import the necessary libraries
-from imblearn.over_sampling import SMOTENC
-from sklearn.utils import resample
-
+# %%
 # Downsample majority class to 50000 samples
-majority_downsampled = resample(data[data.chd==0],
+majority_downsampled = resample(data[data[target]==0],
                                 replace=False, # sample without replacement
                                 n_samples=50000, # number of samples to downsample to
                                 random_state=42) # reproducible results
 
 # Combine minority class and downsampled majority class
-downsampled_data = pd.concat([majority_downsampled, data[data.chd==1]])
+downsampled_data = pd.concat([majority_downsampled, data[data[target]==1]])
 
 # Shuffle the data
 downsampled_data = downsampled_data.sample(frac=1, random_state=42)
 
-X = downsampled_data.drop('chd', axis=1)  # Features (all columns except the target)
-y = downsampled_data['chd']  # Target variable
+X = downsampled_data.drop(target, axis=1)  # Features (all columns except the target)
+y = downsampled_data[target]  # Target variable
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-#get index of categorical features
-cat_cols = data_imputed.select_dtypes(include='object').columns
-cat_cols_idx = [data_imputed.columns.get_loc(col) for col in cat_cols]
 
 # Apply SMOTE to the training data
 smote = SMOTENC(random_state=42, categorical_features = cat_cols_idx)
 X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
 
-# Fit a decision tree classifier to the resampled data
-clf = DecisionTreeClassifier()
-clf.fit(X_resampled, y_resampled)
+# %%
+print("Decision Tree with SMOTENC")
+clf = decision_tree(X_resampled, X_test, y_resampled, y_test, return_clf=True)
+print ("Feature Importances:")
+feature_importance(clf)
 
-# Make predictions on the testing data
-y_pred = clf.predict(X_test)
+print("\nRandom Forest with SMOTENC")
+clf = random_forest(X_resampled, X_test, y_resampled, y_test, return_clf=True)
+print ("Feature Importances:")
+feature_importance(clf)
 
-# Evaluate the performance of the model
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-
-#calculate f1 score
-f1 = f1_score(y_test, y_pred, average='macro')
-print("F1 score:", f1)
-
-report = classification_report(y_test, y_pred)
-print(report)
-
-cm = confusion_matrix(y_test, y_pred)
-print("Confusion Matrix:")
-print(cm)
-#%%
-from sklearn.tree import DecisionTreeClassifier
-# Get feature importances
-importances = clf.feature_importances_
-
-# Print the name and importance of each feature 
-for feature_name, importance in zip(X.columns, importances):
-    print(f"{feature_name}: {importance}")
-#sort features by importance
-sorted(zip(importances, X.columns), reverse=True)
-
-
-#%%
+# %%
 #USE F1 MACRO INSTEAD OF ACCURACY
 #RECALL
 #ROC AND AOC
 #more false positives than false negatives
 #more sensitivisity than specificity instead of false positives/negatives
 
-#%% standup 10/30
+
+# %%
 #consider converting numerical to categorical via bins
 #random forest feature selection
 #one hot encoding
